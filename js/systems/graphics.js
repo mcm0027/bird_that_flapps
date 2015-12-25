6848fabfeb16a555e3c856aaf60a7cecce82d193
Matt Morris <mcm0027@yahoo.com>
2015-12-24 20:04:32 -0600

    add animation and input

--- js/systems/graphics.js
+++ js/systems/graphics.js
@@ -1,22 +1,37 @@
-var GraphicsSystem = function(entities) {
+var GraphicsSystem = function (entities) {
   this.entities = entities;
+  this.canvas = document.getElementById('main-canvas');
+  this.context = this.canvas.getContext('2d');
 };
 
-GraphicsSystem.prototype.run = function() {
-  for(var i=0; i<5; i++) {
-    this.tick();
+GraphicsSystem.prototype.run = function () {
+  window.requestAnimationFrame(this.tick.bind(this));
+};
+
+GraphicsSystem.prototype.tick = function () {
+  if (this.canvas.width != this.canvas.offsetWidth ||
+    this.canvas.height != this.canvas.offsetHeight) {
+    this.canvas.width = this.canvas.offsetWidth;
+    this.canvas.height = this.canvas.offsetHeight;
   }
-};
 
-GraphicsSystem.prototype.tick = function() {
-  for (var i=0; i<this.entities.length; i++) {
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+  this.context.save();
+  this.context.translate(this.canvas.width / 2, this.canvas.height);
+  this.context.scale(this.canvas.height, -this.canvas.height);
+
+
+  for (var i = 0; i < this.entities.length; i++) {
     var entity = this.entities[i];
     if (!'graphics' in entity.components) {
       continue;
     }
-    
     entity.components.graphics.draw(this.context);
   }
+
+  this.context.restore();
+  window.requestAnimationFrame(this.tick.bind(this));
 };
 
 exports.GraphicsSystem = GraphicsSystem;
